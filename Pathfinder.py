26a7e4e0bbac1fa3c8e2f5538c0c31ad87400833
Paarvendhan <ipaar3@gmail.com>
2017-10-28 22:08:35 +0530

    Update Pathfinder.py

--- Pathfinder.py
+++ Pathfinder.py
@@ -13,9 +13,10 @@ print (grid)
 n_points=grid_size**2
 moves=['up','down','left','right']
 learning_rate=0.1
-episodes=100 #should be adjusted in accordance to grid size
+episodes=10 #should be adjusted in accordance to grid size
 max_reward=0.9
-
+#blue dots
+Pitfalls=[5,7,13,19]
 
 #scatterplot format=[[]]
 def scatterplot(x,color):
@@ -25,7 +26,7 @@ def scatterplot(x,color):
 #visualize the change in live
 def update_gui_grid(k):
     plt.scatter(k[0], k[1], c='g',marker='*',s=80)
-    plt.pause(0.01)
+    #plt.pause(0.01)
     pass
 
 #gives possible options for any supplied point in the grid
@@ -92,7 +93,8 @@ def Learner(start,target):
     qtable=pd.DataFrame(np.zeros((n_points,len(moves))),columns=moves)
 
     update_gui_grid(start)
-
+    pit_list=[grid[i] for i in Pitfalls]
+    
     for epi in range(episodes):
 
         #getting point indices
@@ -104,7 +106,9 @@ def Learner(start,target):
 
         while not reached:
             scatterplot(grid,'r')#COMMENT TO RUN FASTER
-
+            scatterplot([target], 'g')
+            scatterplot(pit_list,'b')
+            
             #get the next move that should be made
             move=make_decision(point,qtable)
 
@@ -119,10 +123,15 @@ def Learner(start,target):
                 q_target=1
                 reached=True
 
+            #Negative rewards for pitfalls
+            elif point in Pitfalls:
+                q_target=-1
+
             #else continue to search for target
             else:
                 #selecting the value which has maximum rewards in that point
                 q_target=max_reward*qtable.iloc[next_pt].max()
+                #choosing the max inorder to keep this reward highest for the point
 
             #IMP: updating the q_value for that movement
             qtable.ix[point,move] += learning_rate*(q_target-q_next)
@@ -137,7 +146,10 @@ def Learner(start,target):
 
         #plt.close()
     return qtable
+
 if __name__=='__main__':
+    
     # print obtained QTABLE
     print(Learner([0, 0], [4, 4]))
+    
     plt.show(block=True)
